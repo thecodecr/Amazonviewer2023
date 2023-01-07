139595fae1e3edfbf136a31b6704104cfa547865
Justin Arguello <66757950+thecodecr@users.noreply.github.com>
2023-01-07 11:50:07 -0600

    Add files via upload

---
+++ Chapter.java
@@ -0,0 +1,92 @@
+package com.anncode.amazonviewer.model;
+
+import java.util.ArrayList;
+
+public class Chapter extends Movie {
+	
+	
+	private int id;
+	private int sessionNumber;
+	private Serie serie;
+
+	public Chapter(String title, String genre, String creator, int duration, short year, int sessionNumber, Serie serie) {
+		super(title, genre, creator, duration, year);
+		// TODO Auto-generated constructor stub
+		this.setSessionNumber(sessionNumber);
+		this.setSerie(serie);
+	}
+	
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+	public int getSessionNumber() {
+		return sessionNumber;
+	}
+
+	public void setSessionNumber(int sessionNumber) {
+		this.sessionNumber = sessionNumber;
+	}
+	
+	
+	public Serie getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Serie serie) {
+		this.serie = serie;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return  "\n :: CHAPTER ::" + 
+				"\n Title: " + getTitle() +
+				"\n Year: " + getYear() + 
+				"\n Creator: " + getCreator() +
+				"\n Duration: " + getDuration() +
+				"\n :: SERIE ::" + 
+				"\n Title: " + getSerie().getTitle() ;
+	}
+	
+	
+	public static ArrayList<Chapter> makeChaptersList(Serie serie) {
+		ArrayList<Chapter> chapters = new ArrayList();
+		
+		for (int i = 1; i <= 5; i++) {
+			chapters.add(new Chapter("Capituo "+i, "genero "+i, "creator" +i, 45, (short)(2017+i), i, serie));
+		}
+		
+		return chapters;
+	}
+	
+	@Override
+	public void toSee() {
+		// TODO Auto-generated method stub
+		super.toSee();
+		ArrayList<Chapter> chapters = getSerie().getChapters();
+		int chapterViewedCounter = 0;
+		for (Chapter chapter : chapters) {
+			if (chapter.getIsViewed()) {
+				chapterViewedCounter++;
+			}
+		}
+		if (chapterViewedCounter == chapters.size()) {
+			getSerie().setViewed(true);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
